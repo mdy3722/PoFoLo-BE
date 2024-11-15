a27adb67af6c062161c92822ecbc00dab32e4195
luckyyy <oyslucy@naver.com>
2024-11-15 12:18:42 +0900

    [Fix/#10] url 수정

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
    ProjectListView, ProjectDetailView,
    ProjectCreateView, ProjectUpdateDeleteView, ProjectImageUploadView,
-#   MyProjectsView, LikeProjectView, CommentProjectView
+  MyProjectsView, LikedProjectView, CommentedProjectView
 )
 
 urlpatterns = [
@@ -11,7 +11,7 @@ urlpatterns = [
     path('projects/', ProjectCreateView.as_view(), name='project-create'),  # 프로젝트 생성
     path('projects/upload-images', ProjectImageUploadView.as_view(), name='project-upload-images'), # 이미지 추가    
     path('projects/<int:pk>/', ProjectUpdateDeleteView.as_view(), name='project-detail'), #수정 및 삭제
-    # path('users/projects/myproject/', MyProjectsView.as_view(), name='my-projects'),
-    # path('users/projects/liked/', LikeProjectView.as_view(), name='liked-projects'),
-    # path('users/projects/commented/', CommentProjectView.as_view(), name='commented-projects'),
+    path('users/projects/myproject/', MyProjectsView.as_view(), name='my-projects'),
+    path('users/projects/liked/', LikedProjectView.as_view(), name='liked-projects'),
+    path('users/projects/commented/', CommentedProjectView.as_view(), name='commented-projects'),
 ]
