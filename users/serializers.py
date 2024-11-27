48789412ecdd2bfba8618589c1338282504dfbc0
luckyyy <oyslucy@naver.com>
2024-11-27 14:27:22 +0900

    [Feat] User profile img upload api

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
-from .models import PofoloUser
+from .models import PofoloUser, User
+from django.conf import settings
+from utils import s3_utils
 
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = PofoloUser
     exclude = ['user']
+
+    def update(self, instance, validated_data):
+        # `profile_img` 필드가 있는 경우 처리
+        profile_img_file = self.context['request'].FILES.get('profile_img', None)
+        if profile_img_file:
+            instance.profile_img = s3_utils.s3_file_upload_by_file_data(
+                upload_file=profile_img_file,
+                region_name = settings.AWS_S3_REGION_NAME,
+                bucket_name = settings.AWS_STORAGE_BUCKET_NAME,
+                bucket_path=f"user/profile_images/{instance.id}"
+            )
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
